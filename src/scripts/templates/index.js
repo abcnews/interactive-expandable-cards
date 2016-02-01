0d0f8c7cbaba497b2ffef1b921e2c73874d29f9a
Colin Gourlay <gourlay.colin@abc.net.au>
2016-02-01 16:24:01 +1000

    Removed crucial gaps in template, and switched cards from floated to inline-block.

--- src/scripts/templates/index.js
+++ src/scripts/templates/index.js
@@ -7,7 +7,7 @@ this["JST"]["row"] = Handlebars.template({"1":function(depth0,helpers,partials,d
 },"3":function(depth0,helpers,partials,data,blockParams,depths) {
     var stack1, helper, alias1=this.lambda, alias2=this.escapeExpression, alias3=helpers.helperMissing, alias4="function";
 
-  return "        <a href=\"#ExpandableCards-content--"
+  return "<a href=\"#ExpandableCards-content--"
     + alias2(alias1((depths[1] != null ? depths[1].rowId : depths[1]), depth0))
     + "-"
     + alias2(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(depth0,{"name":"index","hash":{},"data":data}) : helper)))
@@ -30,7 +30,7 @@ this["JST"]["row"] = Handlebars.template({"1":function(depth0,helpers,partials,d
     + alias2(((helper = (helper = helpers.image || (depth0 != null ? depth0.image : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(depth0,{"name":"image","hash":{},"data":data}) : helper)))
     + "\" alt=\"\" /></div>\n            <h3 class=\"ExpandableCards-title\">"
     + alias2(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</h3>\n            <div class=\"ExpandableCards-more\" role=\"presentation\"></div>\n        </a>\n"
+    + "</h3>\n            <div class=\"ExpandableCards-more\" role=\"presentation\"></div>\n        </a>"
     + ((stack1 = helpers['if'].call(depth0,(depths[1] != null ? depths[1].isMobile : depths[1]),{"name":"if","hash":{},"fn":this.program(10, data, 0, blockParams, depths),"inverse":this.noop,"data":data})) != null ? stack1 : "");
 },"4":function(depth0,helpers,partials,data) {
     var helper;
@@ -51,7 +51,7 @@ this["JST"]["row"] = Handlebars.template({"1":function(depth0,helpers,partials,d
 },"10":function(depth0,helpers,partials,data,blockParams,depths) {
     var stack1, helper, alias1=this.lambda, alias2=this.escapeExpression, alias3=helpers.helperMissing, alias4="function";
 
-  return "        <div id=\"ExpandableCards-content-"
+  return "<div id=\"ExpandableCards-content-"
     + alias2(alias1((depths[1] != null ? depths[1].rowId : depths[1]), depth0))
     + "-"
     + alias2(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(depth0,{"name":"index","hash":{},"data":data}) : helper)))
@@ -61,7 +61,7 @@ this["JST"]["row"] = Handlebars.template({"1":function(depth0,helpers,partials,d
     + alias2(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(depth0,{"name":"index","hash":{},"data":data}) : helper)))
     + "\"\n            aria-hidden=\"true\"\n            style=\"height: 0\">\n            <div tabindex=\"0\"><article><div class=\"story richtext\">"
     + ((stack1 = ((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(depth0,{"name":"content","hash":{},"data":data}) : helper))) != null ? stack1 : "")
-    + "</div></article></div>\n        </div>\n";
+    + "</div></article></div>\n        </div>";
 },"12":function(depth0,helpers,partials,data,blockParams,depths) {
     var stack1;
 
@@ -85,9 +85,9 @@ this["JST"]["row"] = Handlebars.template({"1":function(depth0,helpers,partials,d
 
   return "<div class=\"ExpandableCards\">\n    <div class=\"ExpandableCards-row\"\n        role=\"tablist\"\n        "
     + ((stack1 = helpers['if'].call(depth0,(depth0 != null ? depth0.isMobile : depth0),{"name":"if","hash":{},"fn":this.program(1, data, 0, blockParams, depths),"inverse":this.noop,"data":data})) != null ? stack1 : "")
-    + ">\n"
+    + ">"
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.items : depth0),{"name":"each","hash":{},"fn":this.program(3, data, 0, blockParams, depths),"inverse":this.noop,"data":data})) != null ? stack1 : "")
-    + "    </div>\n"
+    + "\n    </div>\n"
     + ((stack1 = helpers.unless.call(depth0,(depth0 != null ? depth0.isMobile : depth0),{"name":"unless","hash":{},"fn":this.program(12, data, 0, blockParams, depths),"inverse":this.noop,"data":data})) != null ? stack1 : "")
     + "</div>\n";
 },"useData":true,"useDepths":true});
