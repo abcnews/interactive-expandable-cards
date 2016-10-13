3691c802c218c02b793c45c8cde7b3467271d7b7
Colin Gourlay <Gourlay.Colin@abc.net.au>
2016-10-13 14:41:19 +1000

    The big ol' Phase 2 upgrade, including some refactoring.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,25 +5,26 @@
  * @author Colin Gourlay <gourlay.colin@abc.net.au>
  */
 
-var dewysiwyg = require('util-dewysiwyg');
-var ns = require('util-news-selectors');
-var templates = {
+const dewysiwyg = require('util-dewysiwyg');
+const ns = require('util-news-selectors');
+const templates = {
 	root: require('../templates/root.hbs'),
-	content: require('../templates/content.hbs')
+	story: require('../templates/story.hbs')
 };
-var TITLE_PREFIX_DELIMETER = ': ';
-var TITLE_PREFIX_REGEX = /^([^:]*)/;
-var TITLE_MINUS_PREFIX_REGEX = /:\s+(.*)/;
-var IMAGE_DIMENSIONS_SRC_SEGMENT_REGEX = /\d+x\d+-\d+x\d+/;
-var THREE_TWO_IMAGE_SRC_SEGMENT = '3x2-460x307';
-var SPACES = /\s/g;
 
-function parseItemFromSection(sectionEl) {
-	var $section, $heading, $img, item;
+const slice = Array.prototype.slice;
 
-	$section = $(sectionEl);
-	$heading = $section.children().first();
-	$img = $heading.next();
+const TITLE_PREFIX_DELIMETER = ': ';
+const TITLE_PREFIX_REGEX = /^([^:]*)/;
+const TITLE_MINUS_PREFIX_REGEX = /:\s+(.*)/;
+const IMAGE_DIMENSIONS_SRC_SEGMENT_REGEX = /\d+x\d+-\d+x\d+/;
+const THREE_TWO_IMAGE_SRC_SEGMENT = '3x2-460x307';
+const SPACES = /\s/g;
+
+const parseItemFromSection = sectionEl => {
+	const $section = $(sectionEl);
+	const $heading = $section.children().first();
+	let $img = $heading.next();
 
 	$heading.detach();
 	$img.detach();
@@ -32,11 +33,11 @@ function parseItemFromSection(sectionEl) {
 		$img = $img.find('img');
 	}
 
-	item = {
+	const item = {
 		title: $heading.text(),
 		image: $img.attr('src').replace(IMAGE_DIMENSIONS_SRC_SEGMENT_REGEX, THREE_TWO_IMAGE_SRC_SEGMENT),
-		$$content: $section.children().filter(function () {
-			return $(this).text() !== ' ';
+		$$children: $section.children().filter((index, el) => {
+			return $(el).text() !== ' ';
 		})
 	};
 
@@ -55,96 +56,120 @@ function parseItemFromSection(sectionEl) {
 	}
 
 	return item;
-}
+};
 
-function triggerResizeEvent() {
-	setTimeout(function () {
-		window[(window.dispatchEvent != null) ? 'dispatchEvent' : 'fireEvent'](new Event('resize'));
-	}, 0);
-}
+const triggerResizeEvent = () => setTimeout(() => {
+	window[(window.dispatchEvent != null) ? 'dispatchEvent' : 'fireEvent'](new Event('resize'));
+}, 0);
 
-function showContent(event) {
-	var $card, $body, $root, $rowEndCard, $activeCard, $content, contentHeight,
-		bodyScrollTop, cardTop, activeCardTop, isStandard, duration;
 
-	$content = event.data.$content;
-	contentHeight = $content.height();
+const showStory = event => {
+	const $target = $(event.target);
+	const $$cards = event.data.$$cards;
+	const $story = event.data.$story;
+	const $card = $target.is('.ExpandableCards-card') ? $target : $target.closest('.ExpandableCards-card');
+	const cardTop = $card.offset().top;
+	const $activeCard = $$cards.filter('.is-active').first();
+	const activeCardTop = $activeCard.length ? $activeCard.offset().top : 0;
+	const $rowEndCard = $$cards.filter((index, el) => $(el).offset().top === cardTop).last();
+	const windowScrollTop = $(window).scrollTop();
+	const $body = $('body');
+	const shouldTransition = $body.width() >= 700;
+	const duration = shouldTransition ? 250 : 0;
 
-	$card = $(this);
-	cardTop = $card.offset().top;
-
-	$root = $card.closest('.ExpandableCards');
-	$rowEndCard = $('.ExpandableCards-card', $root).filter(function () {
-		return $(this).offset().top === cardTop;
-	}).last();
-	$activeCard = $('.ExpandableCards-card.is-active', $root).first();
-	activeCardTop = $activeCard.length ? $activeCard.offset().top : 0;
-
-	$body = $('body');
-	bodyScrollTop = $body.scrollTop();
-	isStandard = $body.hasClass('platform-standard');
-	duration = isStandard ? 250 : 0;
-
-	// Close any active card's related content
-
+	// Close any active card's related story
 	if ($activeCard.length) {
 		$activeCard.removeClass('is-active');
-		$content.slideUp(duration, function () {
-			$activeCard.data('item').$$content.detach();
+		$story.slideUp(duration, () => {
+			$activeCard.data('item').$$children.detach();
 		});
 	}
 
 	// If this card is already active, we're done.
-
 	if ($card.is($activeCard)) {
 		return false;
 	}
 
-	// If it wasn't, make it active and show its content
-
-	setTimeout(function () {
-
-		if (!isStandard && cardTop > activeCardTop) {
-			$body.scrollTop(bodyScrollTop - contentHeight);
-		} else if (isStandard && cardTop < bodyScrollTop) {
+	// If it wasn't, make it active and show its story
+	const show = () => {
+		if (!shouldTransition && activeCardTop > 0 && cardTop > activeCardTop) {
+			$body.scrollTop($card.offset().top - (cardTop - windowScrollTop));
+		} else if (shouldTransition && cardTop < windowScrollTop) {
 			$body.animate({scrollTop: cardTop - 20}, duration);
 		}
 
-		$content.find('.article').append($card.data('item').$$content);
-
-		$rowEndCard.after($content);
-
+		$story.append($card.data('item').$$children);
+		$rowEndCard.after($story);
 		$card.addClass('is-active');
+		$story.slideDown(duration, triggerResizeEvent);
+	};
 
-		$content.slideDown(duration, triggerResizeEvent);
-
-	}, ($activeCard.length ? duration : 0) + 100);
+	if (shouldTransition) {
+		setTimeout(show, ($activeCard.length ? duration : 0) + 100);
+	} else {
+		show();
+	}
 
 	return false;
-}
+};
 
-function wrapSections($teaser) {
-	var sections = [];
-	var stack = [];
+const reorderVisibleStory = event => {
+	const $root = event.data.$root;
+	const $$cards = event.data.$$cards;
+	const $story = event.data.$story;
 
-	function popStack() {
+	if (!$root.has($story).length) {
+		return;
+	}
+
+	const $activeCard = $$cards.filter('.is-active').first();
+
+	if (!$activeCard.length) {
+		return;
+	}
+
+	const numColumns = slice.call($$cards.get()).reduce((memo, el) => {
+		const left = $(el).offset().left;
+
+		if (memo.length === 0 || memo[memo.length - 1] < left) {
+			memo.push(left);
+		}
+
+		return memo;
+	}, []).length;
+
+	const activeCardIndex = $$cards.index($activeCard);
+	const rowEndCardIndex = Math.min($$cards.length - 1,
+		((activeCardIndex + 1) % numColumns) === 0 ?
+		activeCardIndex :
+		activeCardIndex - ((activeCardIndex + 1) % numColumns) + numColumns
+	);
+
+	$($$cards.get(rowEndCardIndex)).after($story);
+};
+
+const wrapSections = $teaser => {
+	const sections = [];
+	let stack = [];
+
+	const popStack = () => {
 		if (!stack.length) {
 			return;
 		}
 
 		sections.push($('<div></div>').append(stack));
 		stack = [];
-	}
+	};
 
 	$teaser.children()
-	.each(function () {
-		var $child = $(this);
+	.each((index, el) => {
+		var $child = $(el);
 
 		if ($child.is('h2')) {
 			popStack();
 		} else if (!stack.length) {
-            return;
-        }
+      return;
+    }
 
 		stack.push($child);
 	});
@@ -153,50 +178,37 @@ function wrapSections($teaser) {
 	$teaser.append(sections);
 
 	return sections;
-}
+};
 
-function init() {
-	var isMobile, $$beacons, $$teasers;
+const init = () => {
+  const $$beacons = $('[data-beacon="interactive-expandable-cards"]', $(ns('story')));
+  const $$teasers = $$beacons.closest(ns('embed:wysiwyg'));
 
-	isMobile = $('body').hasClass('platform-mobile');
-    $$beacons = $('[data-beacon="interactive-expandable-cards"]', $(ns('story')));
-    $$teasers = $$beacons.closest(ns('embed:wysiwyg'));
-    $$beacons.remove();
+	$$beacons.remove();
 
-    $$teasers.each(function (teaserIndex) {
-        var $teaser, sectionEls, items, $root, $content;
+  $$teasers.each((index, el) => {
+    dewysiwyg.normalise(el);
 
-        // Fix the teaser content and normalise across platforms
-        dewysiwyg.normalise(this);
-
-        $teaser = $(this);
-
-        // Wrap sections so they can be grouped and parsed
-        sectionEls = wrapSections($teaser);
-
-		items = sectionEls.map(parseItemFromSection);
-
-		$content = $(templates.content({
-			id: teaserIndex
-		})).hide();
-
-		$root = $(templates.root({
-			id: teaserIndex,
-			isTwoup: true, // TODO: Make optional?
-			items: items
-		}))
-		.on('click', '.ExpandableCards-card', {
-			$content: $content
-		}, showContent);
-
-		$root.find('.ExpandableCards-card')
-		.each(function (itemIndex) {
-			$(this).data('item', items[itemIndex]);
+    const $teaser = $(el);
+    const sectionEls = wrapSections($teaser);
+		const items = sectionEls.map(parseItemFromSection);
+		const $root = $(templates.root({id: index, items: items}));
+		const $$cards = $root.find('.ExpandableCards-card')
+		.each((index, el) => {
+			$(el).data('item', items[index]);
 		});
+		const $story = $(templates.story({id: index})).hide();
+		const refs = {
+			$root: $root,
+			$$cards: $$cards,
+			$story: $story
+		};
 
-        $teaser.empty().append($root);
-    });
+		$root.on('click', '.ExpandableCards-card', refs, showStory);
+		$(window).on('resize', refs, reorderVisibleStory);
+    $teaser.empty().append($root);
+  });
 
-}
+};
 
 init();
