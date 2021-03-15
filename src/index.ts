dd0f99cc3357e8e029a0df085f08596f8f3819fb
Simon Elvery <simon@elvery.net>
2021-03-15 12:42:19 +1000

    Wait for DOM on p1/2 sites

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { getGeneration, GENERATIONS } from '@abcnews/env-utils';
+import { getGeneration, GENERATIONS, whenDOMReady } from '@abcnews/env-utils';
 import { init as legacy } from './legacy';
 import { init } from './pl';
 
 if (getGeneration() === GENERATIONS.PL) init();
-else legacy();
+else whenDOMReady.then(legacy);
