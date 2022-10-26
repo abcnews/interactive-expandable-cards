aea875214490033798f91d93e6ec54d591eb3514
Colin Gourlay <colin@colin-gourlay.com>
2022-10-26 15:24:53 +1000

    Load the legacy module dynamically, if needed

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { getGeneration, GENERATIONS, whenDOMReady } from '@abcnews/env-utils';
-import { init as legacy } from './legacy';
 import { init } from './pl';
 
-if (getGeneration() === GENERATIONS.PL) init();
-else whenDOMReady.then(legacy);
+if (getGeneration() === GENERATIONS.PL) {
+  init();
+} else {
+  const whenLegacyImported = import(/* webpackChunkName: "legacy" */ './legacy');
+
+  Promise.all([whenLegacyImported, whenDOMReady]).then(([{ init }]) => init());
+}
