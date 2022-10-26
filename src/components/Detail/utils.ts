c51ce3adc7f27eabd89f70a6d9a007fb8e03f8ce
Colin Gourlay <colin@colin-gourlay.com>
2022-10-27 09:21:14 +1000

    Add sizes attribute to created detail image. Add src attribute for browsers that don't support srcset (using first rendition).

--- src/components/Detail/utils.ts
+++ src/components/Detail/utils.ts
@@ -1,11 +1,15 @@
 import type { ExpandableCardsImage } from '../ExpandableCards';
 import styles from './styles.scss';
 
+const SIZES = '(min-width: 992px) 50vw, 90vw';
+
 export const createImage = (image: ExpandableCardsImage): HTMLImageElement => {
   const el = document.createElement('img');
 
   el.className = styles.image;
   el.alt = image.alt;
+  el.sizes = SIZES;
+  el.src = image.renditions[0].url;
   el.srcset = image.renditions.map(i => `${i.url} ${i.width}w`).join(', ');
 
   return el;
