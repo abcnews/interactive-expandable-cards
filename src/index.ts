2dff205f6da8d4f9361afe12909d012c30f91175
Simon Elvery <simon@elvery.net>
2023-02-10 16:37:22 +1000

    Add dev-proxy

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
+import { proxy } from '@abcnews/dev-proxy';
 import { getGeneration, GENERATIONS, whenDOMReady } from '@abcnews/env-utils';
 import { init } from './pl';
 
 if (getGeneration() === GENERATIONS.PL) {
-  init();
+  proxy('interactive-expandable-cards').then(init);
 } else {
   const whenLegacyImported = import(/* webpackChunkName: "legacy" */ './legacy');
 
-  Promise.all([whenLegacyImported, whenDOMReady]).then(([{ init }]) => init());
+  Promise.all([whenLegacyImported, whenDOMReady]).then(([{ init }]) =>
+    proxy('interactive-expandable-cards').then(init)
+  );
 }
